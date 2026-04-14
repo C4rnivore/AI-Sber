8c256c37af7fb0d60954853be8e2e0245b906fe7
sancerel <122638737+sancerel@users.noreply.github.com>
2026-04-14 23:52:02 +0500

    create endpoints to stt russian

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from api.dictionary import router as dictionary_router
 from api.translation import router as translation_router
 from api.tts import router as tts_router
+from api.stt import router as stt_router
 
 api_router = APIRouter()
 api_router.include_router(translation_router)
 api_router.include_router(dictionary_router)
 api_router.include_router(tts_router)
+api_router.include_router(stt_router)
